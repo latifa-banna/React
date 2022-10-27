5930ae9f61237ae5b78b697b50b596d6c2184c2a
latifa-banna <latifabanna94@gmail.com>
2022-10-27 13:49:49 +0000

    commit change img salle

--- src/Components/Saleitem.js
+++ src/Components/Saleitem.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 
 function Sale(){
     
-    const P=styled.p`background-color:grey; text-align:center; padding:50px 60px`
+    const IMG=styled.img`background-color:grey; text-align:center; padding:50px 60px`
     const P2=styled.p`text-align:center ;text-size:80px; font-weight:bold`
     const P3=styled.p`text-align:center ;text-size:80px`
     const BUTTON=styled.button`margin-left:5f0px`
     return(
         <div>
             <div>
-                <P><img scr="picture/pictures/salle.jpg" /></P>
+                <IMG scr="picture/pictures/salle.jpg" ></IMG>
                 <P2>Sale Item</P2>
                 <P3><del>$50.00</del>$25.00</P3>
                 <BUTTON>add to cart</BUTTON>
